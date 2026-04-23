ddda41894b5673dbcd81b08f82e61a2d759f99a6
Ifeoluwa Makinde <makindeifeoluwa07@gmail.com>
2026-04-23 14:41:34 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,10 @@
 import { Suspense } from "react";
-import PageHeader from "@/components/PageHeader"; 
+//import PageHeader from "@/components/PageHeader"; 
 import Dashboard from "@/app/dashboard/dashboard";
-
-const time = new Date().toLocaleString("en-US", {
-  hour: "numeric",
-  minute: "numeric",
-  hour12: true,
-});
-
 
 function Page() {
   return (
 <section className="space-y-6">
-      {/*  PAGE HEADER */}
-      <PageHeader
-        title={`${time.includes("AM") ? "Good Morning" : "Good Evening"},\n ${name}`}
-        description="here's your financial intelligence for today"
-      />
  <Suspense fallback={<div>Loading dashboard...</div>}>
         <Dashboard/>
         </Suspense>
